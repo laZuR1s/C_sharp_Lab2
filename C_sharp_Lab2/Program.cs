1382379b9ddb812ad4e11db80e45d11be7157d18
Александр <sashalazyr@gmail.com>
2024-09-15 13:39:51 +0300

    Добавьте файлы проекта.

---
+++ C_sharp_Lab2/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_sharp_Lab2
+{
+    internal class Program
+    {
+
+        static double exactValue(double value)
+        {
+            return 1.0 / Math.Sqrt(1.0 - value * value);
+        }
+
+        static double tailorValue(double value, double eps)
+        {
+            double term = 1;
+            double sum = term;
+            int n = 1;
+
+            while (Math.Abs(term) > eps)
+            {
+                term *= (2 * n - 1.0) * value * value / (2 * n);
+                sum += term;
+                n++;
+            }
+            return sum;
+
+        }
+
+        static void Main(string[] args)
+        {
+            Console.Write("Введите начальное значение X0:");
+            double X0 = double.Parse(Console.ReadLine());
+
+            Console.Write("Введите конечное значение Xn:");
+            double Xn = double.Parse(Console.ReadLine());
+
+            Console.Write("Введите шаг h:");
+            double h = double.Parse(Console.ReadLine());
+
+            Console.Write("Введите шаг eps:");
+            double eps = double.Parse(Console.ReadLine());
+
+            Console.WriteLine($"\n{"x",18} | {"f(x) вычисленное",16} | {"f(x) по формуле",16} |");
+            Console.WriteLine("-----------------------------------------------");
+
+            for (double x = X0; x <= Xn; x += h)
+            {
+                double tailorVal = tailorValue(x, eps);
+
+                double exactVal = exactValue(x);
+
+                Console.WriteLine($"| {x,16:F10} | {tailorVal,16:F10} | {exactVal,16:F10} |");
+            }
+
+
+        }
+    }
+}
---
+++ C_sharp_Lab2/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("C_sharp_Lab2")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("C_sharp_Lab2")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("7963d470-ee41-4a70-85f6-b068d41616b9")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
